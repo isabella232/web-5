ff4fdbd6b2f0fa90b5c5b4b8b6616905ef56cdea
gnormington <gnormington>
2010-10-27 13:19:40 +0000

    edits

--- download/release-notes/1.1.0.RELEASE.php
+++ download/release-notes/1.1.0.RELEASE.php
@@ -30,7 +30,7 @@
 
 <h3>1.1.0.RELEASE - Release Notes</h3>
 
-<p>The 1.1.0 release is the first formal release of Gemini and is aimed at migrating applications and users from the SpringSource OSGi Web Container,
+<p>The 1.1.0 release is the first formal release of Gemini Web and is aimed at migrating applications and users from the SpringSource OSGi Web Container,
 which is the Reference Implementation of the OSGi Web Applications specification.</p>
 
 <h4>Acknowledgements</h4>
@@ -41,30 +41,28 @@ which is the Reference Implementation of the OSGi Web Applications specification
     <li>David Savage (Paremus)</li>
 </ul>
 
-<p>Contributions in the form of bug reports and forum posts were gratefully received from a number of individuals.</p>
+<p>Contributions in the form of forum posts were gratefully received from a number of individuals.</p>
 
 <h4>Highlights</h4>
 
 <p>Code donations (<a href="http://wiki.eclipse.org/Virgo/Source">source code</a>), repackaged to the org.eclipse namespace and dual-licensed under the Eclipse Public License v1.0 and the Apache Software License v2.0:</p>
 <ul>
     <li>Donate SpringSource OSGi Web Container to form Gemini Web Container</li>
-    <li>Donate extensive unit and integration test suites</li>
+    <li>Donate unit and integration test suites</li>
 </ul>
 <p>Dependency upgrades:</p>
 <ul>
-    <li>Upgrade Tomcat to 6.0.29 (plus modifications to support embedding)</li>
+    <li>Upgrade Tomcat to 6.0.29 (plus modifications to support embedding) - closes off some <a href="http://tomcat.apache.org/security-6.html#Fixed_in_Apache_Tomcat_6.0.28">security vulnerabilities</a></li>
     <li>Upgrade SpringSource dm Server utilities to Virgo 2.1.0.RELEASE utilities</li>
 </ul>
 <p>Other improvements and bug fixes:<p/>
 <ul>
-	<li>Support Tomcat context configuration</li>
-	<li>EquinoxBundleFileResolver.canUse throws NoClassDefFoundError when BundleHost cannot be loaded</li>
-	<li>Upgrade to Tomcat 6.0.29 (plus SpringSource mods) to close some <a href="http://tomcat.apache.org/security-6.html#Fixed_in_Apache_Tomcat_6.0.28">security vulnerabilities</a></li>
-	<li>Restructure Tomcat bundles to enable clustering to work &#8211; see <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=322983">bug 322983</a></li>
-	<li>WAR extraction when installing a web application</li>
-	<li>Welcome file in a subdirectory is not invoked</li>
-	<li>PackageInWarScannerTests finds lots more packages in simple-war than before</li>
-	<li>Remove javax.xml.ws dependencies from test configurations and Ivy files</li>
+	<li>Bug 322983: Restructure Tomcat bundles to enable clustering to work</li>
+	<li>Bug 312752: Support Tomcat context configuration</li>
+	<li>Bug 319519: EquinoxBundleFileResolver.canUse throws NoClassDefFoundError when BundleHost cannot be loaded</li>
+	<li>Bug 307393: WAR extraction when installing a web application</li>
+	<li>Bug 308985: Welcome file in a subdirectory is not invoked</li>
+	<li>Bug 319782: Enable Gemini Web to run on Apache Felix</li>
 	<li>Various test and build improvements</li>
 </ul>
 
