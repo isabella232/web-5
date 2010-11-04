ba76aff0b7f8fff294f32f975f21278ed9df2017
gnormington <gnormington>
2010-11-04 13:17:20 +0000

    1.2.0.M01 release notes

---
+++ download/release-notes/1.2.0.M01.php
@@ -0,0 +1,44 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+	$App 	= new App();	
+	$Nav	= new Nav();	
+	$Menu 	= new Menu();		
+	include($App->getProjectCommon());
+	
+/*******************************************************************************
+ * Copyright (c) 2009 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Christopher Frost
+ *    
+ *******************************************************************************/
+
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Gemini Web - Release Notes";
+	$pageKeywords	= "Eclipse, EclipseRT, Virgo, OSGi, Downloads";
+	$pageAuthor		= "Glyn Normington";
+		
+	ob_start();
+?>
+
+<div id="midcolumn">
+
+<h3>1.2.0.M01 - Release Notes</h3>
+ 
+	<p>
+		The purpose of this milestone is to provide a stable base for development on master including
+		cherry-picking all the remaining changes in 1.1.0.RELEASE to master.
+	</p>
+
+</div>
+
+<?
+	$html = ob_get_clean();
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
