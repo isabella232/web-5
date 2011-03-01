8ff58a821fd05a63c518215fb0c50000624dae38
cfrost <cfrost>
2011-03-01 13:10:15 +0000

    Gemini milestone

---
+++ download/release-notes/1.2.0.M03.php
@@ -0,0 +1,46 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+	$App 	= new App();	
+	$Nav	= new Nav();	
+	$Menu 	= new Menu();		
+	include($App->getProjectCommon());
+	
+/*******************************************************************************
+ * Copyright (c) 2009 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Christopher Frost
+ *    
+ *******************************************************************************/
+
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Gemini Web - Release Notes";
+	$pageKeywords	= "Eclipse, EclipseRT, Virgo, OSGi, Downloads";
+	$pageAuthor		= "Glyn Normington";
+		
+	ob_start();
+?>
+
+<div id="midcolumn">
+
+<h3>1.2.0.M03 - Release Notes</h3>
+ 
+	<p>
+		The purpose of this milestone is to provide a consistent base for the 3.0.0.M03 release of Virgo.
+	</p>
+	<p>
+		There were some build problems with 1.2.0.M02 release and there is no zip avaliable
+	</p>
+
+</div>
+
+<?
+	$html = ob_get_clean();
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
