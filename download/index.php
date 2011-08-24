052f9a3838bcfa6f07cd9cda32e0bc84605ed625
cfrost <cfrost>
2011-08-24 13:03:59 +0000

    Adding 2.0.0.RELEASE

--- download/index.php
+++ download/index.php
@@ -42,7 +42,7 @@
 		<h4 class='toggle'>2.0.0.RELEASE</h4>
 		<div class='accordion'>
 			<ul>
-				<li><a href='http://www.eclipse.org/gemini/web/download/release-notes/2.0.0.RELEASE.php' target='_self'>View Release Notes</a> (<a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=200891">Download approved IP log</a>)</li>
+				<li><a href='http://www.eclipse.org/gemini/web/download/release-notes/2.0.0.RELEASE.php' target='_self'>View Release Notes</a> (<a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=200887">Download approved IP log</a>)</li>
 				<li><strong>Gemini Web</strong> -  <a href='http://www.eclipse.org/downloads/download.php?file=/gemini.web/release/GW/2.0.0.RELEASE/gemini-web-2.0.0.RELEASE.zip' target='_self'>Download</a></li>
 				<li>Documentation is included with the download and can be viewed on line <a href="http://www.eclipse.org/gemini/web/documentation/">here</a>.
 			</ul>
