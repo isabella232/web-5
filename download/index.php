158e66e2819cc7de0607917ffdcdecc77850296a
vgeorgieva <vgeorgieva>
2011-10-24 21:21:08 +0000

    Adding p2 repository information

--- download/index.php
+++ download/index.php
@@ -45,6 +45,7 @@
 			<ul>
 				<li><a href='http://www.eclipse.org/gemini/web/download/release-notes/2.0.1.RELEASE.php' target='_self'>View Release Notes</a> (<a href="https://bugs.eclipse.org/bugs/attachment.cgi?id=200887">Download approved IP log</a>)</li>
 				<li><strong>Gemini Web</strong> -  <a href='http://www.eclipse.org/downloads/download.php?file=/gemini.web/release/GW/2.0.1.RELEASE/gemini-web-2.0.1.RELEASE.zip' target='_self'>Download</a></li>
+				<li><strong>Gemini Web p2 repository</strong> - <a href='http://download.eclipse.org/gemini/updates/web/2.0.1/' target='_self'>http://download.eclipse.org/gemini/updates/web/2.0.1/</a></li>
 				<li>Documentation is included with the download and can be viewed on line <a href="http://www.eclipse.org/gemini/web/documentation/">here</a>.
 			</ul>
 		</div>
