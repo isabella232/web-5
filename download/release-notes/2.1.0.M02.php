2d2eaeec5c6f7278019334e266bed9b4506a2891
vgeorgieva <vgeorgieva>
2012-03-04 16:13:22 +0000

    In addition to previous change - corrected bug list filter.

--- download/release-notes/2.1.0.M02.php
+++ download/release-notes/2.1.0.M02.php
@@ -30,7 +30,7 @@
 
 <h3>2.1.0.M02 - Release Notes</h3>
 <p>This milestone has mainly bug fixes.</p>
-<p>Bug list is available <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;classification=RT;product=Gemini.Web;target_milestone=2.1.0.M01" target="_self">here</a>.</p>
+<p>Bug list is available <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;classification=RT;product=Gemini.Web;target_milestone=2.1.0.M02" target="_self">here</a>.</p>
 
 </div>
 
