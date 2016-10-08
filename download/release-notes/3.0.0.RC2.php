6a47679e50d426bf67db7151fe34866927dcdd7e
Violeta Georgieva <vgeorgieva@pivotal.io>
2016-10-08 22:48:09 +0300

    Added information for the new 3.0.0.RC2

---
+++ download/release-notes/3.0.0.RC2.php
@@ -0,0 +1,52 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App 	= new App();
+	$Nav	= new Nav();
+	$Menu 	= new Menu();
+	include($App->getProjectCommon());
+
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Violeta Georgieva
+ *
+ *******************************************************************************/
+
+	# Begin: page-specific settings.  Change these.
+	$pageTitle 		= "Gemini Web - Release Notes";
+	$pageKeywords	= "Eclipse, EclipseRT, Virgo, OSGi, Downloads";
+	$pageAuthor		= "Violeta Georgieva";
+
+	ob_start();
+?>
+
+<div id="midcolumn">
+
+<h3>3.0.0.RC2 - Release Notes</h3>
+
+<h4>Notable improvements:</h4>
+
+<ul>
+	<li>Embedded Apache Tomcat version 8.5.5.</li>
+</ul>
+<p><p>
+<h4>Migration from Gemini Web 3.0.0.RC1 to Gemini Web 3.0.0.RC2</h4>
+<p>
+<p>Bundles with changed version</p>
+<ul>
+	<li>All Apache Tomcat bundles 8.0.21 -> 8.5.5</li>
+</ul>
+
+</div>
+
+<?
+	$html = ob_get_clean();
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
