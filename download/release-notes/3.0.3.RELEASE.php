19d29a22b5426c3d60e07317a87a4e4db3061f04
Violeta Georgieva <vgeorgieva@pivotal.io>
2017-06-29 08:28:23 +0300

    Update 3.0.3.RELEASE notes

--- download/release-notes/3.0.3.RELEASE.php
+++ download/release-notes/3.0.3.RELEASE.php
@@ -32,7 +32,30 @@
 
 <p>The 3.0.3 release of Gemini Web upgrades Gemini Web to pick up Apache Tomcat 8.5.16.</p>
 <p>
+<h4>Notable Improvements</h4>
+
+<ul>
+	<li>Upgrade to Apache Tomcat 8.5.16</li>
+</ul>
+
 <p>A full list of bugs included in this release is available <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=2407161;classification=RT;query_format=advanced;component=unknown;target_milestone=3.0.3.RELEASE;product=Gemini.Web" target="_self">here</a>.</p>
+<p>
+<h4>Migration from Gemini Web 3.0.0 to Gemini Web 3.0.3</h4>
+<h5>Bundles with upgraded version</h5>
+<ul>
+	<li>org.apache.catalina</li>
+	<li>org.apache.catalina.ha</li>
+	<li>org.apache.catalina.tribes</li>
+	<li>org.apache.coyote</li>
+	<li>org.apache.el</li>
+	<li>org.apache.jasper</li>
+	<li>org.apache.tomcat.api</li>
+	<li>org.apache.tomcat.util</li>
+	<li>org.apache.tomcat.websocket</li>
+	<li>org.apache.juli</li>
+	<li>org.apache.tomcat.jni</li>
+	<li>org.apache.tomcat.util.scan</li>
+</ul>
 <p><a href="3.0.0.RELEASE.php" target="_self">3.0.0.RELEASE - Release Notes</a></p>
 <p><a href="3.0.1.RELEASE.php" target="_self">3.0.1.RELEASE - Release Notes</a></p>
 <p><a href="3.0.2.RELEASE.php" target="_self">3.0.2.RELEASE - Release Notes</a></p>
